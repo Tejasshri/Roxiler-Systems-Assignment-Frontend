887eac53f81f54f3d02e55f06ccfb5264360cd13
tejasshri <tejasshrishekhar08@gmail.com>
2023-12-03 08:33:12 +0530

    first commit

---
+++ src/components/TransactionsStatistics/index.js
@@ -0,0 +1,23 @@
+import "./index.css";
+
+export default function TransactionsStatistics({ statistics, monthName }) {
+  return (
+    <section className="transactions-statistics">
+      <h2 className="statistics-title">Statistics - {monthName}</h2>
+      <div className="statistics-container">
+        <div className="statistics-item">
+          <p>Total Sale</p>
+          <p>{statistics.sales}</p>
+        </div>
+        <div className="statistics-item">
+          <p>Total sold items</p>
+          <p>{statistics.soldItems}</p>
+        </div>
+        <div className="statistics-item">
+          <p>Total not sold items</p>
+          <p>{statistics.unSoldItems}</p>
+        </div>
+      </div>
+    </section>
+  );
+}
